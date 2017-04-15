54316ae209d93794d546ec5db473890d72059b18
Jaschar Domann <jaschar.doman@posteo.de>
2017-04-15 13:15:23 +0200

    Add first monads react and reflex * add React interface * add Reflex interface

---
+++ core/src/main/java/com/github/jasjisdo/lemonad/Reflex.java
@@ -0,0 +1,127 @@
+package com.github.jasjisdo.lemonad;
+
+import java.io.Serializable;
+import java.util.function.Consumer;
+
+/**
+ *
+ */
+public interface Reflex<T> {
+
+    default Reflex<T> thenRun(Runnable runnable) {
+        if(this.isActive()) {
+            runnable.run();
+        }
+        return this;
+    }
+
+    default Reflex<T> orElseRun(Runnable runnable) {
+        if(this.isIdle()) {
+            runnable.run();
+        }
+        return this;
+    }
+
+    default Reflex<T> then(Consumer<T> consumer) {
+        if(this.isActive()) {
+            consumer.accept(getSense());
+        }
+        return this;
+    }
+
+    default Reflex<T> orElse(Consumer<T> consumer) {
+        if(this.isIdle()) {
+            consumer.accept(getSense());
+        }
+        return this;
+    }
+
+    T getSense();
+
+    boolean isActive();
+
+    boolean isIdle();
+
+    class Basic<T> implements Reflex<T>, Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        private final T value;
+
+        /**
+         * Constructs a Success.
+         *
+         * @param value The value of this Success.
+         */
+        public Basic(T value) {
+            this.value = value;
+        }
+
+        @Override
+        public T getSense() {
+            return value;
+        }
+
+        @Override
+        public boolean isActive() {
+            return false;
+        }
+
+        @Override
+        public boolean isIdle() {
+            return false;
+        }
+
+    }
+
+    final class Active<T> extends Basic<T> implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * Constructs a Success.
+         *
+         * @param value The value of this Success.
+         */
+        Active(T value) {
+            super(value);
+        }
+
+        @Override
+        public boolean isActive() {
+            return true;
+        }
+
+        @Override
+        public boolean isIdle() {
+            return false;
+        }
+
+    }
+
+    final class Idle<T> extends Basic<T> implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * Constructs a Success.
+         *
+         * @param value The value of this Success.
+         */
+        Idle(T value) {
+            super(value);
+        }
+
+        @Override
+        public boolean isActive() {
+            return false;
+        }
+
+        @Override
+        public boolean isIdle() {
+            return true;
+        }
+
+    }
+
+}
---
+++ core/src/test/java/com/github/jasjisdo/lemonad/ReflexTest.java
@@ -0,0 +1,43 @@
+package com.github.jasjisdo.lemonad;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.function.Predicate;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Created by domann on 15.04.17.
+ */
+public class ReflexTest {
+
+    private ByteArrayOutputStream baos;
+
+    @Before
+    public void setUp() throws Exception {
+        baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        System.setOut(ps);
+    }
+
+    @Test
+    public void whenPredicateIsFulfilled_thenPrintStringToOut() throws Exception {
+        String A = "A";
+        Predicate<String> isCapsA = s -> s.equals("A");
+        React.on(A).when(isCapsA).then(System.out::print);
+        assertThat(baos.toString(), is("A"));
+    }
+
+    @Test
+    public void whenPredicateIsNotFulfilled_thenPrintNothingToOut() throws Exception {
+        String a = "a";
+        Predicate<String> isCapsA = s -> s.equals("A");
+        React.on(a).when(isCapsA).then(System.out::print);
+        assertThat(baos.toString(), is(""));
+    }
+}
