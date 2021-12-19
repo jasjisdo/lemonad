98dd416d8d06cfab7978bea0b15cec8bebf0d112
Jaschar Domann <jaschar.domann@posteo.de>
2021-12-19 20:09:44 +0100

    replaces log4j classic with most recent logback
    - replaces log4j dependencies with logback dependencies.
    - adds slf4j api dependency
    - adds logback.xml to classpath.
    - adds logback-test.xml to classpath.
    - adds info log message when stringList is created during unit test setup.

--- core/src/test/java/com/github/jasjisdo/lemonad/ForTest.java
+++ core/src/test/java/com/github/jasjisdo/lemonad/ForTest.java
@@ -2,6 +2,8 @@ package com.github.jasjisdo.lemonad;
 
 
 import com.github.jasjisdo.lemonad.loop.For;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,10 +13,12 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 
 public class ForTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ForTest.class);
 
     private final List<String> stringList = new ArrayList<>();
     private final Predicate<String> isNotEmpty = s -> !s.isEmpty();
@@ -32,6 +36,8 @@ public class ForTest {
         stringList.add("Joe2");
         stringList.add("Doe1");
         stringList.add("Doe2");
+
+        logger.info("created stringList = {}", stringList);
 
     }
 
